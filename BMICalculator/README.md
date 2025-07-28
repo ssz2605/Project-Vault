511d87b3dc2d808bc16de6e5efd7b8dde3c492b4
Shreya Saxena <shreyasaxena2605@gmail.com>
2025-07-28 09:48:22 +0530

    Added BMI Calculator project

---
+++ BMICalculator/README.md
@@ -0,0 +1,39 @@
+# 🧮 BMI Calculator
+
+A beautifully styled Body Mass Index (BMI) Calculator built with **React.js**.  
+It uses a modern **glassmorphism UI** and calculates BMI based on user input height and weight.
+
+
+---
+
+## 🚀 Features
+
+- Live BMI Calculation
+- Category Detection (Underweight, Normal, Overweight, Obese)
+- Responsive Layout
+- Glassmorphism UI Design
+- Built with React and CSS
+
+---
+
+## 📦 Tech Stack
+
+- React.js (with Vite)
+- CSS3 (custom design)
+- JavaScript (ES6)
+
+---
+
+
+
+---
+
+## 🛠️ Installation
+
+Clone the repo and run locally:
+
+```bash
+git clone https://github.com/yourusername/bmi-calculator.git
+cd bmi-calculator
+npm install
+npm run dev
---
+++ BMICalculator/src/BMICalculator.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import "./BMICalculator.css";
+
+function BMICalculator() {
+    const [height, setHeight] = useState("");
+    const[weight, setWeight] = useState("");
+    const [bmi, setBmi] = useState(null);
+    const [category, setCategory] = useState("");
+
+    const calculateBMI = ()=>{
+        if(height && weight){
+            const heightInMeters = height / 100;
+            const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+            setBmi(bmi);
+
+            if(bmi < 18.5){
+                setCategory("Underweight");
+            }
+            else if(bmi >= 18.5 && bmi <= 24.9){
+                setCategory("Normal weight");
+            }else if(bmi >= 25 && bmi <= 29.9){
+                setCategory("Overweight");
+            }else{
+                setCategory("Obese");
+            }
+        }
+    }
+
+  return (
+    <div className='bmi-card'>
+        <h2>BMI Calculator</h2>
+        <div className='input-group'>
+            <label>Weight (kg)</label>
+            <input 
+             type='number'
+             value={weight}
+             onChange={(e) => setWeight(e.target.value)}
+             placeholder='Enter your weight'
+            />
+        </div>
+        <div className='input-group'>
+            <label>Height (cm)</label>
+            <input
+                type='number'
+                value={height}
+                onChange={(e) => setHeight(e.target.value)}
+                placeholder='Enter your height'
+            />
+        </div>
+        <button className='btn-calculator' onClick={calculateBMI}>Calculate BMI</button>
+        {
+            bmi && (
+                <div className='result'>
+                    <h3>Your BMI: {bmi}</h3>
+                    <h4>Category: {category}</h4>
+                </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default BMICalculator
